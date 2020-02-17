7c14697c65772e12666754ff8a40dda3e4b4726e
AlejandroVillarreal <alexvodark@hotmail.com>
2020-02-17 12:15:09 -0600

    version 1.2
    Solo falta inciso B

--- cifrador.py
+++ cifrador.py
@@ -41,11 +41,12 @@ def generar_cifrador(offset):
 
 
 #Completa esta funcion (quita # para activar)
+print(os.getcwd())
 try:
     archivo = open("nombre.txt", "r")
     lista = archivo.readline()
     archivo.close()
-    print(lista)
+    print(f"Nombre: {lista}")
 except IOError as identifier:
     print("No se pudo leer el archivo")
     sys.exit(1)
@@ -53,25 +54,33 @@ except IOError as identifier:
 
 def cifrar(palabra, cifrador):
     letras = [item for item in palabra]
-    print(letras)
     #print(cifrador.values())
     palabra_cifrada=[]
     for letra in palabra:
         palabra_cifrada.append(cifrador.get(letra))
-    print(palabra_cifrada)
-    print(' '.join(palabra_cifrada))
+    print(f"Nombre cifrado: {''.join(palabra_cifrada)}")
 
-    
-    
+def conentenidoCifrador(cifrador):
+    for item in cesar3:
+        print(f"Letra: {item}  Letra cifrada: {cesar3[item]}")
 
-def curp():
-    nombre = ["a","l","e","x"," ","v","i","l","l","a", " ","o","c","a"]
-    curp = []
-    #print(nombre)
-    for letra in nombre:
-        nombre.pop()
-    print(nombre)
-curp()
+
+def curp(palabra):
+    nombre = palabra.split(" ")
+    ap = list(nombre[2])
+    am = list(nombre[3])
+    nom = list(nombre[0])
+    _curp = []
+    _curp.append(str(ap[0]).upper())
+    _curp.append(str(ap[1]).upper())
+    _curp.append(str(am[0]).upper())
+    _curp.append(str(nom[0]).upper())
+    print(f"Codigo para CURP o RFC: {''.join(_curp)}")
+
+cifrar(lista,cesar3)
+curp(lista)
+#cifrar(lista,cesar3)
+conentenidoCifrador(cesar3)
 #cifrar(lista, cesar3)
 #Completa esta funcion (quita # para activar)
 #def mostrar_cifrador(cifrador):
